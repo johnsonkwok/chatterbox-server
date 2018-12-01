cf311c3022b2db7f3e46d2e6de665b3026e3f345
johnsonkwok <johnsonkwok1@gmail.com>
2018-11-30 20:02:19 -0800

    Allow the server to send back an object and accept POST requests
    * Allow server to send back parsable stringified JSON

--- server/request-handler.js
+++ server/request-handler.js
@@ -27,32 +27,92 @@ exports.requestHandler = function(request, response) {
   // Adding more logging to your server can be an easy way to get passive
   // debugging help, but you should always be careful about leaving stray
   // console.logs in your code.
-  console.log('Serving request type ' + request.method + ' for url ' + request.url);
 
+  const { method, url } = request;
+  const headers = {
+    'access-control-allow-origin': '*',
+    'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+    'access-control-allow-headers': 'content-type, accept',
+    'access-control-max-age': 10
+  };
+  let results = [];
+
+  request.on('error', (err) => {
+    console.error(err);
+  });
+  response.on('error', (err) => {
+    console.error(err);
+  });
+  
+
+  if (request.method === 'GET') {
+    request.on('data', (chunk) => {
+      results.push(chunk);
+    }).on('end', () => {
+      results = results.concat(results);
+    
+      var statusCode = 200;
+      headers['Content-Type'] = 'application/json';
+      response.writeHead(statusCode, headers);
+      
+      const responseBody = { results };
+      response.end(JSON.stringify(responseBody));
+    });
+  } else if (request.method === 'POST') {
+    request.on('data', (chunk) => {
+      results.push(chunk);
+    }).on('end', () => {
+      results = results.concat(results);
+    
+      var statusCode = 201;
+      headers['Content-Type'] = 'application/json';
+      response.writeHead(statusCode, headers);
+      
+      const responseBody = { results };
+      response.end(JSON.stringify(responseBody));
+    });
+  } else {
+    response.statusCode = 404;
+    response.end();
+  }
+
+  // console.log('Serving request type ' + method + ' for url ' + url);
   // The outgoing status.
-  var statusCode = 200;
-
+  // var statusCode = 200;
+  
   // See the note below about CORS headers.
-  var headers = defaultCorsHeaders;
+  // var headers = defaultCorsHeaders;
 
-  // Tell the client we are sending them plain text.
-  //
-  // You will need to change this if you are sending something
-  // other than plain text, like JSON or HTML.
-  headers['Content-Type'] = 'text/plain';
+  // // Tell the client we are sending them plain text.
+  // //
+  // // You will need to change this if you are sending something
+  // // other than plain text, like JSON or HTML.
+  // headers['Content-Type'] = 'text/plain';
+  // // JSON: application/json
+  // // HTML: text/html 
 
-  // .writeHead() writes to the request line and headers of the response,
-  // which includes the status and all headers.
-  response.writeHead(statusCode, headers);
+  // // .writeHead() writes to the request line and headers of the response,
+  // // which includes the status and all headers.
+  // response.writeHead(statusCode, headers);
 
-  // Make sure to always call response.end() - Node may not send
-  // anything back to the client until you do. The string you pass to
-  // response.end() will be the body of the response - i.e. what shows
-  // up in the browser.
-  //
-  // Calling .end "flushes" the response's internal buffer, forcing
-  // node to actually send all the data over to the client.
-  response.end('Hello, World!');
+  // // Make sure to always call response.end() - Node may not send
+  // // anything back to the client until you do. The string you pass to
+  // // response.end() will be the body of the response - i.e. what shows
+  // // up in the browser.
+  // //
+  // // Calling .end "flushes" the response's internal buffer, forcing
+  // // node to actually send all the data over to the client.
+  // response.end('Hello, World!');
+
+  // 
+  // if (request.method === 'GET') {
+  //   response.writeHead(200, {'Content-Type': 'text/plain'});
+  //   response.end('eggplant');
+  // } else if (request.method === 'POST') {
+  //   response.writeHead(200, {'Content-Type': 'text/plain'});
+  //   response.end('Success');
+  // }
+  
 };
 
 // These headers will allow Cross-Origin Resource Sharing (CORS).
@@ -64,10 +124,10 @@ exports.requestHandler = function(request, response) {
 //
 // Another way to get around this restriction is to serve you chat
 // client from this domain by setting up static file serving.
-var defaultCorsHeaders = {
-  'access-control-allow-origin': '*',
-  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
-  'access-control-allow-headers': 'content-type, accept',
-  'access-control-max-age': 10 // Seconds.
-};
+// var defaultCorsHeaders = {
+//   'access-control-allow-origin': '*',
+//   'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+//   'access-control-allow-headers': 'content-type, accept',
+//   'access-control-max-age': 10 // Seconds.
+// };
 
